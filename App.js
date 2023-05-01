c419c8f440622f63a4b8660e16a2f1c8a189eaa3
fe-ktch <sangarefantha@gmail.com>
2023-05-01 19:26:18 +0200

    finish project

--- App.js
+++ App.js
@@ -2,49 +2,54 @@ import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { useEffect, useState } from 'react';
+const data = require('./database.json');
 
 export default class App extends Component {
   constructor(props) {
-  super(props);
-  this.state = {
-    HeadTable: ['Head1', 'Head2', 'Head3', 'Head4', 'Head5'],
-    DataTable: [
-      ['1', '2', '3', '4', '5'],
-      ['a', 'b', 'c', 'd', 'e'],
-      ['1', '2', '3', '4', '5'],
-      ['a', 'b', 'c', 'd', 'e'],
-      ['1', '2', '3', '4', '5']
-    ]
+    super(props);
+    const dataTable = data.bikes.map(bike => [bike.id, bike.name, bike.wheel, bike.usage, bike.price]);
+    this.state = {
+      HeadTable: ['#', 'Name', 'Wheel', 'Usage', 'Price'],
+      DataTable: dataTable
+    };
   }
-}
 
-render() {
-  const state = this.state;
-  return (
-    <View style={styles.container}>
-      <Table borderStyle={{borderWidth: 1, borderColor: '#ffa1d2'}}>
-        <Row data={state.HeadTable} style={styles.HeadStyle} textStyle={styles.TableText}/>
-        <Rows data={state.DataTable} textStyle={styles.TableText}/>
-      </Table>
-    </View>
-  )
-}
-
+  render() {
+    const state = this.state;
+    return (
+      <View style={styles.container}>
+        <Table>
+          <Row data={state.HeadTable} style={styles.HeadStyle} textStyle={styles.TableHead}/>
+          <Rows borderStyle={{borderWidth: 0, borderColor: '#'}} data={state.DataTable} textStyle={styles.TableText}/>
+        </Table>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: { 
     flex: 1,
-    padding: 18,
+    padding: 15,
     paddingTop: 35,
-    backgroundColor: '#ffffff' 
+    backgroundColor: '#EEEEEE',
   },
   HeadStyle: { 
     height: 50,
     alignContent: "center",
-    backgroundColor: '#ffe0f0'
+    backgroundColor: '#FFCD32',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+
+  },
+  TableHead: {
+    padding: 12,
+    alignContent: "center",
+    color: '#FFFFFF',
   },
   TableText: { 
-    margin: 10
-  }
+    margin: 10,
+    color: '#474040',
+  },
 });
